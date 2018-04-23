c6129ffc31746eb26bdfccb424222d2e8f974b44
Akinwale Ariwodola <akinwale@gmail.com>
2018-04-23 10:26:09 +0100

    added new setting to toggle background media play

--- app/src/component/mediaPlayer/index.js
+++ app/src/component/mediaPlayer/index.js
@@ -1,7 +1,11 @@
 import { connect } from 'react-redux';
+import { SETTINGS } from 'lbry-redux';
+import { makeSelectClientSetting } from '../../redux/selectors/settings';
 import MediaPlayer from './view';
 
-const select = state => ({});
+const select = state => ({
+    backgroundPlayEnabled: makeSelectClientSetting(SETTINGS.BACKGROUND_PLAY_ENABLED)(state),
+});
 const perform = dispatch => ({});
 
 export default connect(select, perform)(MediaPlayer);
