ca9772691aaf62d560f6284ac3cf2455f8d36148
Akinwale Ariwodola <akinwale@gmail.com>
2018-04-24 16:50:00 +0100

    first run time tracking implementation

--- app/src/page/discover/view.js
+++ app/src/page/discover/view.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import FeaturedCategory from '../../component/featuredCategory';
 import NavigationActions from 'react-navigation';
-import { Text, View, ScrollView } from 'react-native';
+import { AsyncStorage, NativeModules, ScrollView, Text, View } from 'react-native';
 import discoverStyle from '../../styles/discover';
 import Feather from 'react-native-vector-icons/Feather';
 
 class DiscoverPage extends React.PureComponent {
   componentWillMount() {
     this.props.fetchFeaturedUris();
+    
+    // Track the total time taken if this is the first launch
+    AsyncStorage.getItem('firstLaunchTime').then(start => {
+      if (start !== null && !isNaN(start)) {
+        // We know this is the first app launch because firstLaunchTime is set and it's a valid number
+        const now = moment().unix();
+        const delta = now - start;
+        AsyncStorage.getItem('firstLaunchSuspended').then(suspended => {
+          if (NativeModules.Mixpanel) {
+            NativeModules.Mixpanel.track('First Run Time', {
+              totalSeconds: delta,
+              appSuspended: !!suspended
+            });
+          }
+        });
+      }
+    });
   }
 
   render() {
