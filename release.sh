fd90f37f820667a0e7c253b05581d022d9ef4d31
Akinwale Ariwodola <akinwale@gmail.com>
2018-05-09 18:33:31 +0100

    fix build-tools path

--- release.sh
+++ release.sh
@@ -11,7 +11,7 @@ jarsigner -verbose -sigalg SHA1withRSA \
     -storepass $KEYSTORE_PASSWORD \
     bin/browser-$version-release-unsigned.apk lbry-android \
     && mv bin/browser-$version-release-unsigned.apk bin/browser-$version-release-signed.apk
-~/.buildozer/platform//SDKs/android/build-tools/26.0.1/zipalign -v 4 \
+~/.buildozer/android/platform/android-sdk-23/build-tools/26.0.1/zipalign -v 4 \
     bin/browser-$version-release-signed.apk bin/browser-$version-release.apk \
     && rm bin/browser-$version-release-signed.apk
 
