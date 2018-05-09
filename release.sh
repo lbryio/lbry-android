5c1e542dab6f409c0be70eda869fded5f5ccbb33
akinwale <akinwale@gmail.com>
2018-05-09 19:26:57 +0100

    Travis CI deployment to AWS and Github releases (#114)

--- release.sh
+++ release.sh
@@ -6,11 +6,11 @@ version=$(cat src/main/python/main.py | grep --color=never -oP '([0-9]+\.?)+')
 buildozer android release
 jarsigner -verbose -sigalg SHA1withRSA \
     -digestalg SHA1 \
-    -keystore ~/.keystores/lbry-android.keystore \
+    -keystore lbry-android.keystore \
     -storepass $KEYSTORE_PASSWORD \
     bin/browser-$version-release-unsigned.apk lbry-android \
     && mv bin/browser-$version-release-unsigned.apk bin/browser-$version-release-signed.apk
-~/Dev/SDKs/android/build-tools/26.0.2/zipalign -v 4 \
+~/.buildozer/android/platform/android-sdk-23/build-tools/26.0.1/zipalign -v 4 \
     bin/browser-$version-release-signed.apk bin/browser-$version-release.apk \
     && rm bin/browser-$version-release-signed.apk
 
