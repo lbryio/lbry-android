453adb444a6f9a737466b261e4539c1d2f0d8081
Akinwale Ariwodola <akinwale@gmail.com>
2018-05-10 21:46:19 +0100

    temp fix for builds failing due to gmplib.org being unreachable

--- recipes/libgmp/__init__.py
+++ recipes/libgmp/__init__.py
@@ -7,7 +7,8 @@ import sh
 
 class LibGMPRecipe(Recipe):
     version = '6.1.2'
-    url = 'https://gmplib.org/download/gmp/gmp-{version}.tar.bz2'
+    url = 'https://ftp.gnu.org/pub/gnu/gmp/gmp-{version}.tar.bz2'
+    #url = 'https://gmplib.org/download/gmp/gmp-{version}.tar.bz2'
 
     def should_build(self, arch):
         build_dir = self.get_build_dir(arch.arch)
