ed61f470f78793583a363f569c9af184463b181a
Akinwale Ariwodola <akinwale@gmail.com>
2018-05-25 19:52:08 +0100

    Implement channel page with just the first page of results (#140)

--- app/src/page/channel/index.js
+++ app/src/page/channel/index.js
@@ -1,19 +1,22 @@
 import { connect } from 'react-redux';
 import {
+  doFetchClaimsByChannel,
+  doFetchClaimCountByChannel,
   makeSelectClaimForUri,
-  makeSelectClaimsInChannelForCurrentPage,
+  makeSelectClaimsInChannelForPage,
   makeSelectFetchingChannelClaims,
 } from 'lbry-redux';
 import ChannelPage from './view';
 
 const select = (state, props) => ({
   claim: makeSelectClaimForUri(props.uri)(state),
-  claimsInChannel: makeSelectClaimsInChannelForCurrentPage(props.uri)(state),
+  claimsInChannel: makeSelectClaimsInChannelForPage(props.uri, props.page || 1)(state),
   fetching: makeSelectFetchingChannelClaims(props.uri)(state),
 });
 
 const perform = dispatch => ({
-
+  fetchClaims: (uri, page) => dispatch(doFetchClaimsByChannel(uri, page)),
+  fetchClaimCount: uri => dispatch(doFetchClaimCountByChannel(uri)),
 });
 
 export default connect(select, perform)(ChannelPage);
--- app/src/page/channel/view.js
+++ app/src/page/channel/view.js
@@ -1,20 +1,54 @@
 // @flow
 import React from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { ActivityIndicator, Text, View } from 'react-native';
+import Colors from '../../styles/colors';
+import FileList from '../../component/fileList';
+import PageHeader from '../../component/pageHeader';
 import UriBar from '../../component/uriBar';
 import channelPageStyle from '../../styles/channelPage';
 
 class ChannelPage extends React.PureComponent {
+  componentDidMount() {
+    const { uri, page, claimsInChannel, fetchClaims, fetchClaimCount } = this.props;
+
+    if (!claimsInChannel || !claimsInChannel.length) {
+      fetchClaims(uri, page || 1);
+      fetchClaimCount(uri);
+    }
+  }
+
   render() {
-    const { claim, navigation, uri } = this.props;
-    const { name } = claim;
+    const { fetching, claimsInChannel, claim, navigation, uri } = this.props;
+    const { name, permanent_url: permanentUrl } = claim;
+
+    let contentList;
+    if (fetching) {
+      contentList = (
+        <View style={channelPageStyle.busyContainer}>
+          <ActivityIndicator size="large" color={Colors.LbryGreen} />
+          <Text style={channelPageStyle.infoText}>Fetching content...</Text>
+        </View>
+      );
+    } else {
+      contentList =
+        claimsInChannel && claimsInChannel.length ? (
+          <FileList sortByHeight
+                    hideFilter
+                    fileInfos={claimsInChannel}
+                    navigation={navigation}
+                    style={channelPageStyle.fileList} />
+        ) : (
+          <View style={channelPageStyle.busyContainer}>
+            <Text style={channelPageStyle.infoText}>No content found.</Text>
+          </View>
+        );
+    }
+
 
     return (
       <View style={channelPageStyle.container}>
-        <Text style={channelPageStyle.title}>{name}</Text>
-        <ScrollView style={channelPageStyle.content}>
-          
-        </ScrollView>
+        <PageHeader title={name} onBackPressed={() => { this.props.navigation.goBack(); }} />
+        {contentList}
         <UriBar value={uri} navigation={navigation} />
       </View>
     )
