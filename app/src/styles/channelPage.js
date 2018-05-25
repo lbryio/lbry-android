33eeae5b883778c64eff0234957df5a7b7d7f1be
Akinwale Ariwodola <akinwale@gmail.com>
2018-05-25 12:39:36 +0100

    Implement channel page with just the first page of results

--- app/src/styles/channelPage.js
+++ app/src/styles/channelPage.js
@@ -9,11 +9,27 @@ const channelPageStyle = StyleSheet.create({
   content: {
     flex: 1
   },
+  fileList: {
+    paddingTop: 30,
+    flex: 1
+  },
   title: {
     color: Colors.LbryGreen,
     fontFamily: 'Metropolis-SemiBold',
     fontSize: 30,
     margin: 16
+  },
+   busyContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  infoText: {
+    fontFamily: 'Metropolis-Regular',
+    fontSize: 20,
+    textAlign: 'center',
+    marginLeft: 10
   }
 });
 
