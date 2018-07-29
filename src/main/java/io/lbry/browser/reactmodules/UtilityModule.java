bfefc53040d53c56d2f3b7c9a0f30d788cbca58e
Akinwale Ariwodola <akinwale@gmail.com>
2018-07-29 09:12:45 +0100

    hide navigation bar in fullscreen playback mode

--- src/main/java/io/lbry/browser/reactmodules/UtilityModule.java
+++ src/main/java/io/lbry/browser/reactmodules/UtilityModule.java
@@ -2,6 +2,7 @@ package io.lbry.browser.reactmodules;
 
 import android.app.Activity;
 import android.content.Context;
+import android.view.View;
 import android.view.WindowManager;
 
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -23,29 +24,51 @@ public class UtilityModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void keepAwakeOn() {
-      final Activity activity = getCurrentActivity();
+        final Activity activity = getCurrentActivity();
 
-      if (activity != null) {
-        activity.runOnUiThread(new Runnable() {
-          @Override
-          public void run() {
-            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-          }
-        });
-      }
+        if (activity != null) {
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                  activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                }
+            });
+        }
     }
 
     @ReactMethod
     public void keepAwakeOff() {
-      final Activity activity = getCurrentActivity();
+        final Activity activity = getCurrentActivity();
 
-      if (activity != null) {
-        activity.runOnUiThread(new Runnable() {
-          @Override
-          public void run() {
-            activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-          }
-        });
-      }
+        if (activity != null) {
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                }
+            });
+        }
+    }
+
+    @ReactMethod
+    public void hideNavigationBar() {
+        if (context != null && context instanceof Activity) {
+            Activity activity = (Activity) context;
+            View decorView = activity.getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                                            View.SYSTEM_UI_FLAG_IMMERSIVE |
+                                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        }
+    }
+
+    @ReactMethod
+    public void showNavigationBar() {
+        if (context != null && context instanceof Activity) {
+            Activity activity = (Activity) context;
+            View decorView = activity.getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                                            View.SYSTEM_UI_FLAG_VISIBLE);
+        }
     }
 }
