745aa614664420526a068a7ccd372652233353cc
Akinwale Ariwodola <akinwale@gmail.com>
2018-08-08 16:22:56 +0100

    Delete and Stop download button tweaks

--- app/src/component/button/view.js
+++ app/src/component/button/view.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 import buttonStyle from '../../styles/button';
+import Colors from '../../styles/colors';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class Button extends React.PureComponent {
@@ -10,9 +11,10 @@ export default class Button extends React.PureComponent {
       style,
       text,
       icon,
+      theme,
       onPress
     } = this.props;
-    
+
     let styles = [buttonStyle.button, buttonStyle.row];
     if (style) {
       if (style.length) {
@@ -21,19 +23,25 @@ export default class Button extends React.PureComponent {
         styles.push(style);
       }
     }
-    
+
     if (disabled) {
       styles.push(buttonStyle.disabled);
     }
-    
+
     const textStyles = [buttonStyle.text];
     if (icon && icon.trim().length > 0) {
       textStyles.push(buttonStyle.textWithIcon);
     }
-    
+    if (theme === 'light') {
+      textStyles.push(buttonStyle.textDark);
+    } else {
+      // Dark background, default
+      textStyles.push(buttonStyle.textLight);
+    }
+
     return (
       <TouchableOpacity disabled={disabled} style={styles} onPress={onPress}>
-        {icon && <Icon name={icon} size={18} color='#ffffff' class={buttonStyle.icon} /> }
+        {icon && <Icon name={icon} size={18} color={'light' === theme ? Colors.DarkGrey : Colors.White} />}
         {text && (text.trim().length > 0) && <Text style={textStyles}>{text}</Text>}
       </TouchableOpacity>
     );
