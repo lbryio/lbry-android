4e14e958272db967bd4ca15b0dd132bab7e88c6d
Akinwale Ariwodola <akinwale@gmail.com>
2018-08-31 23:28:07 +0100

    attempt travis build fix

--- release.sh
+++ release.sh
@@ -8,10 +8,10 @@ jarsigner -verbose -sigalg SHA1withRSA \
     -digestalg SHA1 \
     -keystore lbry-android.keystore \
     -storepass $KEYSTORE_PASSWORD \
-    bin/browser-$version-release-unsigned.apk lbry-android \
+    bin/browser-$version-release-unsigned.apk lbry-android > /dev/null \
     && mv bin/browser-$version-release-unsigned.apk bin/browser-$version-release-signed.apk
 ~/.buildozer/android/platform/android-sdk-23/build-tools/26.0.1/zipalign -v 4 \
-    bin/browser-$version-release-signed.apk bin/browser-$version-release.apk \
+    bin/browser-$version-release-signed.apk bin/browser-$version-release.apk > /dev/null \
     && rm bin/browser-$version-release-signed.apk
 
 
