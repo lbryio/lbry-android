28bff7c5fcc7bf79de95c62eda7e6ed107df62fe
Akinwale Ariwodola <akinwale@gmail.com>
2018-09-28 23:11:16 +0100

    add setuptools requirement to zope.interface recipe

--- recipes/zope_interface/__init__.py
+++ recipes/zope_interface/__init__.py
@@ -9,7 +9,7 @@ class ZopeInterfaceRecipe(PythonRecipe):
     url = 'https://pypi.python.org/packages/source/z/zope.interface/zope.interface-{version}.tar.gz'
     site_packages_name = 'zope.interface'
 
-    depends = [('python2', 'python3crystax')]
+    depends = [('python2', 'python3crystax'), 'setuptools']
     patches = ['no_tests.patch']
 
     def prebuild_arch(self, arch):
