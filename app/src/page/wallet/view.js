0f5003ec02f3af54d3d56341e927c712db6c29d9
Akinwale Ariwodola <akinwale@gmail.com>
2019-03-29 18:35:14 +0100

    fix NativeModules import on wallet page

--- app/src/page/wallet/view.js
+++ app/src/page/wallet/view.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { NativeModules, ScrollView, Text, View } from 'react-native';
 import TransactionListRecent from 'component/transactionListRecent';
 import WalletAddress from 'component/walletAddress';
 import WalletBalance from 'component/walletBalance';
