434dfe47b2fe051c46d632dbffcc3e15b4d40d90
Akinwale Ariwodola <akinwale@gmail.com>
2019-08-22 15:52:36 +0100

    app head: edit published content

--- src/main/java/io/lbry/browser/MainActivity.java
+++ src/main/java/io/lbry/browser/MainActivity.java
@@ -37,7 +37,7 @@ import com.facebook.react.modules.core.PermissionListener;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.react.ReactRootView;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
-import com.reactnativedocumentpicker.ReactNativeDocumentPicker;
+import io.github.elyx0.reactnativedocumentpicker.DocumentPickerPackage;
 import com.rnfs.RNFSPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -148,7 +148,7 @@ public class MainActivity extends Activity implements DefaultHardwareBackBtnHand
                 .addPackage(new MainReactPackage())
                 .addPackage(new AsyncStoragePackage())
                 .addPackage(new FastImageViewPackage())
-                .addPackage(new ReactNativeDocumentPicker())
+                .addPackage(new DocumentPickerPackage())
                 .addPackage(new ReactVideoPackage())
                 .addPackage(new ReanimatedPackage())
                 .addPackage(new RNCameraPackage())
