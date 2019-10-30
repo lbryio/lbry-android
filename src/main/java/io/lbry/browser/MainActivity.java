6e6ed07890bb9dd6caa1fc12a54293a787daac51
Akinwale Ariwodola <akinwale@gmail.com>
2019-10-30 10:33:09 +0100

    app head: rc

--- src/main/java/io/lbry/browser/MainActivity.java
+++ src/main/java/io/lbry/browser/MainActivity.java
@@ -80,7 +80,7 @@ public class MainActivity extends Activity implements DefaultHardwareBackBtnHand
     private static final int PHONE_STATE_PERMISSION_REQ_CODE = 202;
 
     private static final int RECEIVE_SMS_PERMISSION_REQ_CODE = 203;
-    
+
     public static final int DOCUMENT_PICKER_RESULT_CODE = 301;
 
     private BroadcastReceiver notificationsReceiver;
@@ -297,7 +297,7 @@ public class MainActivity extends Activity implements DefaultHardwareBackBtnHand
         };
         registerReceiver(smsReceiver, smsFilter);
     }
-    
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
@@ -307,7 +307,7 @@ public class MainActivity extends Activity implements DefaultHardwareBackBtnHand
                 }
             }
         }
-        
+
         if (requestCode == DOCUMENT_PICKER_RESULT_CODE) {
             ReactContext reactContext = mReactInstanceManager.getCurrentReactContext();
             if (reactContext != null) {
@@ -637,7 +637,7 @@ public class MainActivity extends Activity implements DefaultHardwareBackBtnHand
 
         return array;
     }
-    
+
     /**
      * https://gist.github.com/HBiSoft/15899990b8cd0723c3a894c1636550a8
      */
