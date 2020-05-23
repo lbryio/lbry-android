26703815b2e95f9b5cbf5109400b26c6973913b7
Akinwale Ariwodola <akinwale@gmail.com>
2020-05-23 07:51:56 +0100

    cleanup top level directory

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd app
-react-native bundle --platform android --dev false --entry-file src/index.js --bundle-output ../src/main/assets/index.android.bundle --assets-dest ../src/main/res/
-cd ..
-buildozer android debug
