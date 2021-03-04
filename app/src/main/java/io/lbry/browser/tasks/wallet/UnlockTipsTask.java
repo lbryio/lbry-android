e762d1ad10fdf09e60b975c38da5182ed030b549
Javi Rueda <foss@franjaru.com>
2021-03-04 22:00:53 +0100

    Fix mass tips unlocking

--- app/src/main/java/io/lbry/browser/tasks/wallet/UnlockTipsTask.java
+++ app/src/main/java/io/lbry/browser/tasks/wallet/UnlockTipsTask.java
@@ -2,24 +2,16 @@ package io.lbry.browser.tasks.wallet;
 
 import android.os.AsyncTask;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import io.lbry.browser.exceptions.ApiCallException;
-import io.lbry.browser.model.WalletBalance;
 import io.lbry.browser.tasks.GenericTaskHandler;
-import io.lbry.browser.utils.Helper;
 import io.lbry.browser.utils.Lbry;
 
 public class UnlockTipsTask extends AsyncTask<Void, Void, Boolean> {
 
-    private GenericTaskHandler handler;
+    private final GenericTaskHandler handler;
     private Exception error;
 
     public UnlockTipsTask(GenericTaskHandler handler) {
@@ -28,34 +20,18 @@ public class UnlockTipsTask extends AsyncTask<Void, Void, Boolean> {
 
     public Boolean doInBackground(Void... params) {
 
-        List<String> txids = new ArrayList<>();
-
         try {
+            // TODO Consolidate transactions when there are more than 500 UTXO
+
             Map<String, Object> options = new HashMap<>();
             options.put("type", "support");
             options.put("is_not_my_input", true);
-            options.put("is_my_output", true);
-            JSONObject result = (JSONObject) Lbry.genericApiCall(Lbry.METHOD_TXO_LIST, options);
-            if (result.has("items") && !result.isNull("items")) {
-                JSONArray items = result.getJSONArray("items");
-                for (int i = 0; i < items.length(); i++) {
-                    JSONObject item = items.getJSONObject(i);
-                    String txid = Helper.getJSONString("txid", null, item);
-                    if (!Helper.isNullOrEmpty(txid)) {
-                        txids.add(txid);
-                    }
-                }
-            }
+            options.put("blocking", true);
 
-            if (txids.size() > 0) {
-                options = new HashMap<>();
-                options.put("txid", txids);
-                options.put("blocking", true);
-                Lbry.genericApiCall(Lbry.METHOD_TXO_SPEND, options);
-            }
+            Lbry.genericApiCall(Lbry.METHOD_TXO_SPEND, options);
 
             return true;
-        } catch (ApiCallException | ClassCastException | JSONException ex) {
+        } catch (ApiCallException | ClassCastException ex) {
             error = ex;
             return false;
         }
